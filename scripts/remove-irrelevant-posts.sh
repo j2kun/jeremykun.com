4dfc39303ae24d63128d8261267495a7fbf794b0
Jeremy Kun <j2kun@users.noreply.github.com>
2018-11-09 20:27:34 -0800

    start tracking scripts for reproducibility

---
+++ scripts/remove-irrelevant-posts.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# not sure what these files are doing here. They seem to be identical copies
+rm content/_posts/*markdown--
+
+# remove posts that are no longer relevant (nobody would want to read them today)
+rm content/_posts/2012-07-17-blog-referral-economics-arbitrage-schemes-and-team-fortress-2.markdown
+rm content/_posts/2012-12-30-2012-in-review-at-math-e288a9-programming.markdown
+rm content/_posts/2013-04-04-support-math-e288a9-programming-t-shirts-mugs-donations.markdown
+rm content/_posts/2013-04-27-persistent-homology-talk-at-uic-slides.markdown
+rm content/_posts/2013-05-05-facebook-page-google-community-and-whispers-of-guest-posts.markdown
+rm content/_posts/2013-06-10-why-theoretical-computer-scientists-arent-worried-about-privacy.markdown
+rm content/_posts/2013-05-13-dog-print-available-for-sale.markdown
+rm content/_posts/2013-06-03-conferences-summer-work-and-an-advisor.markdown
+rm content/_posts/2013-06-27-status-update-and-boston-python-project-night.markdown
+rm content/_posts/2014-03-24-where-math-e288a9-programming-is-headed.markdown
+rm content/_posts/2014-04-21-midwest-theory-of-computing-day-at-purdue-university.markdown
+rm content/_posts/2014-06-12-three-years-old-and-an-idea-for-a-podcast.markdown
+rm content/_posts/2014-07-02-ams-network-science-mathematical-research-community.markdown
+rm content/_posts/2015-01-08-joint-mathematics-meeting.markdown
+rm content/_posts/2015-05-11-math-intersect-programming-survey.markdown
+rm content/_posts/2015-08-10-support-math-e288a9-programming-by-becoming-a-patron.markdown
+rm content/_posts/2016-04-25-book-mailing-list.markdown
+rm content/images/2015-08-10-support-math-e288a9-programming-by-becoming-a-patron/patreon-logo.jpg
+rm content/images/2014-07-02-ams-network-science-mathematical-research-community/10500522_608259311336_2296185156899262778_n.jpg
+rm content/images/2012-07-17-blog-referral-economics-arbitrage-schemes-and-team-fortress-2/tf2items.jpg
+rm content/images/2012-12-30-2012-in-review-at-math-e288a9-programming/2012-emailteaser.png
+rm content/images/2013-05-13-dog-print-available-for-sale/bezier-dog-for-web.png
+rm content/images/2014-03-24-where-math-e288a9-programming-is-headed/blog-map1.png
